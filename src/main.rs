47494e0d6e2d5043f193be364317d81ea37c0195
PentaWasTaken <piet.helzel@outlook.com>
2020-12-07 21:11:18 +0100

    Demonstrates current functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::Read;
+
+mod ram;
+use ram::Ram;
+
+mod chip8;
+use chip8::Chip8;
+
+const ROM_PATH: &str = "games/danm8ku.ch8";
+
 fn main() {
-    println!("Hello, world!");
+    //Read the ROM file
+    let mut file = File::open(ROM_PATH).expect(&format!("ROM '{}' not found!", ROM_PATH));
+    let mut data: Vec<u8> = Vec::new();
+    file.read_to_end(&mut data).expect("Could not read file.");
+
+    //Create a new Chip8 instance and load the rom into RAM
+    let mut chip8 = Chip8::new();
+    chip8.load_rom(&data);
+
+    println!("{:?}", chip8);
 }
